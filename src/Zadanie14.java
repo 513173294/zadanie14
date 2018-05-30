929645268a9ab52acfa149ea5799a82d0bf60773
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-05-31 01:24:42 +0200

    zadanie 14 done

--- src/Zadanie14.java
+++ src/Zadanie14.java
@@ -8,22 +8,21 @@ public class Zadanie14 {
         Scanner scanner = new Scanner(System.in);
 
         String[] imia = new String[5];
-int a = 0;
+        int a = 0;
 
 
-while (a<imia.length){
-        System.out.println("podaj imie");
-        imia[0+a] = scanner.nextLine();
-        a++;
-
-
+        while (a < imia.length) {
+            System.out.println("podaj imie");
+            imia[0 + a] = scanner.nextLine();
+            a++;
         }
-        System.out.println("cześć " + imia[4]);
-        System.out.println("cześć " + imia[3]);
-        System.out.println("cześć " + imia[2]);
-        System.out.println("cześć " + imia[1]);
-        System.out.println("cześć " + imia[0]);
-        System.out.println(Arrays.toString (imia));
+        int x = 0;
+        while (x < 5) {
+            System.out.println("cześć " + imia[4 - x]);
+            x++;
+        }
+        System.out.println(Arrays.toString(imia));
+
 
     }
 }
